b1f8f9fd3c1856a137dfe903b69da204a30c6ce6
InJung Chung <mu29gl@gmail.com>
2015-07-02 17:06:34 +0900

    Fix routes

--- unistudy.py
+++ unistudy.py
@@ -4,8 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/register/')
+def register():
+    return render_template('register.html')
 
 @app.route('/contact/')
 def contact():
