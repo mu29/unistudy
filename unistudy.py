641041d8391d3e22f9e68ac8cc32dfa359665dfd
InJung Chung <mu29gl@gmail.com>
2015-07-08 14:13:18 +0900

    Complete login function

--- unistudy.py
+++ unistudy.py
@@ -1,5 +1,8 @@
+#-*- coding: utf-8 -*-
+
 from flask import *
 from flaskext.mysql import MySQL
+import MySQLdb
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -7,30 +10,54 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'projectDanbi'
 app.config['MYSQL_DATABASE_DB'] = 'unistudy'
+app.secret_key = 'secretkey'
 
 mysql.init_app(app)
 
 @app.route('/')
 def index():
-    if 'username' in session:
+    if 'email' in session:
         return render_template('index.html')
     return redirect(url_for('login'))
 
 @app.route('/login/', methods=['POST', 'GET'])
 def login():
+    error = None
     if request.method == 'POST':
-        if do_login(request.form['email'], request.form['password']):
+        error = do_login(request.form['email'], request.form['password'])
+        if error is None:
             session['email'] = request.form['email']
             return redirect(url_for('index'))
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout/')
+def logout():
+    session.pop('email', None)
     return render_template('login.html')
 
 def do_login(email, password):
-    cursor = mysql.connect().cursor()
+    # 아이디와 비밀번호로 유저 검색
+    cursor = mysql.connect().cursor(MySQLdb.cursors.DictCursor)
     cursor.execute("SELECT * FROM `user` WHERE `email` = '" + email + "' AND `password` = '" + password + "';")
 
-    if len(cursor.fetchall()) > 0:
-        return True
-    return False
+    # 유저가 없을 경우
+    if cursor.rowcount == 0:
+        return u'아이디와 비밀번호를 확인하세요.'
+
+    # 유저를 얻자
+    userdata = cursor.fetchone()
+
+    # 인증이 안된 경우
+    if userdata['verify'] == 0:
+        return u'가입 인증 메일을 확인해주세요.'
+    # 밴 된 아이디일 경우
+    if userdata['ban'] == 1:
+        return u'차단된 계정입니다.'
+
+    # 로그인 성공
+    return None
 
 
 @app.route('/register/')
