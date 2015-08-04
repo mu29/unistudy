0958e2b4c4c33d8b4ebdb9f17a366f514dc48e3b
InJung Chung <mu29gl@gmail.com>
2015-08-04 22:40:01 +0900

    Markdown test

--- lecture.py
+++ lecture.py
@@ -1,9 +1,9 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from flask import *
-from database import db
 from server import app
 
 @app.route('/lecture/<subject>')
 def lecture(subject):
+    #content = Markup(markdown.markdown(content))
     return render_template('lecture.html', subject=subject)
