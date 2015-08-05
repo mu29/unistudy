41cf9842868cac17241795e6351ae9e2e480f4cf
InJung Chung <mu29gl@gmail.com>
2015-08-05 14:13:17 +0900

    Load lecture list from database

--- database/lecture_db.py
+++ database/lecture_db.py
@@ -1,30 +1,46 @@
 # -*- coding: utf-8 -*-
 
 import MySQLdb
+import markdown
+from flask import Markup
 from database.db import *
 from server import app
+
 
 class LectureDB:
 
     def __init__(self):
         self.connection = db.get_mysql().connect()
         self.connection.autocommit(True)
-        self.subjects = self.get_subjects()
+        self.subject_list = self.get_subjects()
 
     def get_subjects(self):
         cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute("SELECT * FROM `subjects`;")
-        list = cursor.fetchall()
+        subject_list = cursor.fetchall()
         cursor.close()
 
-        return list
+        for subject in subject_list:
+            subject['message'] = Markup(markdown.markdown(subject['message']))
+
+        return subject_list
+
+    def get_lectures(self, subject_id):
+        cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM `lectures` WHERE `subject_id` = " + str(subject_id) + ";")
+        lecture_list = cursor.fetchall()
+        cursor.close()
+
+        return lecture_list
 
 
 lecture_db = LectureDB()
 
+
 @app.template_global()
 def subjects():
-    return lecture_db.subjects
+    return lecture_db.subject_list
+
 
 @app.template_global()
 def get_length(item):
--- lecture.py
+++ lecture.py
@@ -2,8 +2,17 @@
 
 from flask import *
 from server import app
+from database.lecture_db import lecture_db
 
-@app.route('/lecture/<subject>')
-def lecture(subject):
-    #content = Markup(markdown.markdown(content))
-    return render_template('lecture.html', subject=subject)
+
+@app.route('/lecture/<no>')
+def lecture(no):
+    # content = Markup(markdown.markdown(content))
+
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
+    no = int(no)
+    subject_id = lecture_db.subject_list[no]['id']
+    return render_template('lecture.html', subject_info=lecture_db.subject_list[no],
+                           lecture_info=lecture_db.get_lectures(subject_id))
